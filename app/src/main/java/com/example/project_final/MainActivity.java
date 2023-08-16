37c2d8763153629e6a9c78f6c5327ee20a03bb70
CcesdeJeff <jcarvajalbe@cesde.net>
2023-08-15 21:53:39 -0500

    Se hace la logica del botón

--- app/src/main/java/com/example/project_final/MainActivity.java
+++ app/src/main/java/com/example/project_final/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.project_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       siguiente=(Button)findViewById(R.id.button);
+
+       siguiente.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i = new Intent(MainActivity.this,Principal.class);
+                startActivity(i);
+           }
+       });
     }
 }
